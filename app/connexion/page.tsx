e007ef5bf0aabd58944635f7c98e52474d67ee4d
silby <silamakankamissoko@gmail.com>
2026-01-31 01:14:55 +0100

    Fix login/profile warnings

--- app/connexion/page.tsx
+++ app/connexion/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 import Connexion from "./content";
  
 export const metadata: Metadata = {
@@ -6,5 +7,9 @@ export const metadata: Metadata = {
 };
  
 export default function ConnexionPage() {
-  return <Connexion />;
+  return (
+    <Suspense fallback={null}>
+      <Connexion />
+    </Suspense>
+  );
 }
